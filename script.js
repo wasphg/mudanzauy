99741804ac8c980fe53b6d6e423c85e88bd1cd4f
CaroMercury <09200228@unmsm.edu.pe>
2024-05-16 14:15:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,331 @@
+const data_products = {
+    products: [
+      {
+        Info: {
+          id: 1,
+          mainImage: "images/licuadora_1.jpg",
+          images: ["images/licuadora_1.jpg", "images/licuadora_2.jpg", "images/licuadora_3.jpg", "images/licuadora_4.jpg"],
+          title: "LICUADORA PHILIPS 2L 1200W Negra",
+          description: "Licuadora 2L 1200W. Modelo: HR2242 PRO BLEND6. Jarra: 2L. Material: Jarra plástico. Alto: 39,5 cm. Ancho: 26,8 cm. Potencia: 1200W. Color: Negro. Número de velocidades: 12. Voltaje: 220V. Origen: Brasil. Estilo: Industrial. Tiempo de uso 2 meses: Pocas veces usada. Comprada en Sodimac: sodimac.com.uy/sodimac-uy/product/3027635/licuadora-2-l-1200-w-negra/3027635. Precio original: USD 165",
+          price: "$2500"
+        }
+      },
+      {
+        Info: {
+          id: 2,
+          mainImage: "images/estufa_1.jpg",
+          images: ["images/estufa_1.jpg", "images/estufa_2.jpg", "images/estufa_3.jpg"],
+          title: "Estufa JAMES",
+          description: "Estufa marca James. Uso 1 año y medio. ",
+          price: "$2500"
+        }
+      }, 
+      {
+        Info: {
+          id: 3,
+          mainImage: "images/radiador_1.jpg",
+          images: ["images/radiador_1.jpg", "images/radiador_2.jpg"],
+          title: "Estufa JAMES",
+          description: "Radiador JAMES. Como nuevo ",
+          price: "$2000"
+        }
+      },    
+      {
+        Info: {
+          id: 4,
+          mainImage: "images/echeveria_fred_ives_crestada_1.jpg",
+          images: ["images/echeveria_fred_ives_crestada_1.jpg", "images/echeveria_fred_ives_crestada_2.jpg", "images/echeveria_fred_ives_crestada_3.jpg"],
+          title: "Echeveria Fred Ives Crestada + maceta + tierra",
+          description: "Echeveria Fred Ives Crestada + maceta + tierra",
+          price: "$700"
+        }
+      },        
+      {
+        Info: {
+          id: 5,
+          mainImage: "images/echeveria_laui_1.jpg",
+          images: ["images/echeveria_laui_1.jpg", "images/echeveria_laui_2.jpg"],
+          title: "Echeveria Laui + maceta + tierra",
+          description: "Echeveria Laui + maceta + tierra",
+          price: "$2000"
+        }
+      },
+      {
+        Info: {
+          id: 6,
+          mainImage: "images/begonia_maculata_1.jpg",
+          images: ["images/begonia_maculata_1.jpg", "images/begonia_maculata_2.jpg", "images/begonia_maculata_3.jpg", "images/begonia_maculata_4.jpg"],
+          title: "Begonia Maculata Grande + maceta + tierra",
+          description: "Begonia Maculata grande + maceta + tierra",
+          price: "$2500"
+        }
+      },
+      {
+        Info: {
+          id: 7,
+          mainImage: "images/begonia_maculata_2_1.jpg",
+          images: ["images/begonia_maculata_2_1.jpg", "images/begonia_maculata_2_2.jpg"],
+          title: "Begonia Maculata mediana + maceta + tierra",
+          description: "Begonia Maculata mediana con flor + maceta + tierra",
+          price: "$1500"
+        }
+      },        
+      {
+        Info: {
+          id: 8,
+          mainImage: "images/begonia_maculata_4_1.jpg",
+          images: ["images/begonia_maculata_4_1.jpg", "images/begonia_maculata_4_2.jpg"],
+          title: "Begonia Maculata pequeña + maceta + tierra",
+          description: "Begonia Maculata pequeña + maceta + tierra",
+          price: "$1000"
+        }
+      },      
+      {
+        Info: {
+          id: 9,
+          mainImage: "images/begonia_maculata_5_1.jpg",
+          images: ["images/begonia_maculata_5_1.jpg", "images/begonia_maculata_5_2.jpg", "images/begonia_maculata_5_3.jpg"],
+          title: "Begonia Maculata pequeña + maceta + tierra",
+          description: "Begonia Maculata pequeña + maceta + tierra",
+          price: "$500"
+        }
+      },
+      {
+        Info: {
+          id: 10,
+          mainImage: "images/begonia_maculata_3_1.jpg",
+          images: ["images/begonia_maculata_3_1.jpg", "images/begonia_maculata_3_2.jpg","images/begonia_maculata_3_3.jpg", "images/begonia_maculata_3_4.jpg"],
+          title: "Begonia Maculata mediana + maceta + tierra",
+          description: "Begonia Maculata mediana + maceta + tierra",
+          price: "$1500"
+        }
+      }, 
+      {
+        Info: {
+          id: 11,
+          mainImage: "images/echeveria_beatrice_1.jpg",
+          images: ["images/echeveria_beatrice_1.jpg", "images/echeveria_beatrice_2.jpg", "images/echeveria_beatrice_3.jpg", "images/echeveria_beatrice_4.jpg"],
+          title: "Echeveria Beatrice + maceta + tierra",
+          description: "Echeveria Beatrice grande + maceta + tierra",
+          price: "$600"
+        }
+      }, 
+      {
+        Info: {
+          id: 12,
+          mainImage: "images/echeveria_linda_jane_1_1.jpg",
+          images: ["images/echeveria_linda_jane_1_1.jpg", "images/echeveria_linda_jane_1_2.jpg", "images/echeveria_linda_jane_1_3.jpg", "images/echeveria_linda_jane_1_4.jpg", "images/echeveria_linda_jane_1_5.jpg"],
+          title: "Echeveria Linda Jane (grande) + maceta + tierra",
+          description: "Echeveria Linda Jane grande + maceta + tierra",
+          price: "$1000"
+        }
+      }, 
+      {
+        Info: {
+          id: 13,
+          mainImage: "images/echeveria_copahue_1.jpg",
+          images: ["images/echeveria_copahue_1.jpg", "images/echeveria_copahue_2.jpg"],
+          title: "Echeveria Copahue + maceta + tierra",
+          description: "Echeveria Copahue + maceta + tierra",
+          price: "$800"
+        }
+      },
+      {
+        Info: {
+          id: 14,
+          mainImage: "images/echeveria_valkirye_1.jpg",
+          images: ["images/echeveria_valkirye_1.jpg", "images/echeveria_valkirye_2.jpg","images/echeveria_valkirye_3.jpg"],
+          title: "Echeveria Valkirye grande + maceta + tierra",
+          description: "Echeveria Copahue + maceta + tierra",
+          price: "$2000"
+        }
+      },
+      {
+        Info: {
+          id: 15,
+          mainImage: "images/dudleya_brittonii_1.jpg",
+          images: ["images/dudleya_brittonii_1.jpg", "images/dudleya_brittonii_2.jpg"],
+          title: "Dudleya Brittonii + maceta + tierra",
+          description: "Dudleya Brittonii + maceta + tierra",
+          price: "$600"
+        }
+      },    
+      {
+        Info: {
+          id: 16,
+          mainImage: "images/echeveria_lilacina_1.jpg",
+          images: ["images/echeveria_lilacina_1.jpg", "images/echeveria_lilacina_2.jpg", "images/echeveria_lilacina_3.jpg", "images/echeveria_lilacina_4.jpg"],
+          title: "Echeveria Lilacina + maceta + tierra",
+          description: "Echeveria Lilacina + maceta + tierra",
+          price: "$800"
+        }
+      },        
+      {
+        Info: {
+          id: 17,
+          mainImage: "images/caly_argentea_1.jpg",
+          images: ["images/caly_argentea_1.jpg", "images/caly_argentea_2.jpg"],
+          title: "Echeveria Caly Argentea + maceta + tierra",
+          description: "Echeveria Caly Argentea grande + maceta + tierra: $800. Echeveria Caly Argentea pequeña + maceta + tierra: $400",
+          price: "$800"
+        }
+      },        
+      {
+        Info: {
+          id: 18,
+          mainImage: "images/echeveria_shaviana_1.jpg",
+          images: ["images/echeveria_shaviana_1.jpg", "images/echeveria_shaviana_2.jpg"],
+          title: "Echeveria Shaviana + maceta + tierra",
+          description: "Echeveria Shaviana + maceta + tierra",
+          price: "$800"
+        }
+      },        
+      {
+        Info: {
+          id: 19,
+          mainImage: "images/echeveria_shaviana_2_1.jpg",
+          images: ["images/echeveria_shaviana_2_1.jpg", "images/echeveria_shaviana_2_2.jpg"],
+          title: "Echeveria Shaviana pequeña + maceta + tierra",
+          description: "Echeveria Shaviana pequeña + maceta + tierra",
+          price: "$500"
+        }
+      },        
+      {
+        Info: {
+          id: 20,
+          mainImage: "images/cactus_1.jpg",
+          images: ["images/cactus_1.jpg", "images/cactus_2.jpg"],
+          title: "Cactus pequeño + maceta + tierra",
+          description: "Cactus pequeño + maceta + tierra",
+          price: "$300"
+        }
+      },        
+      {
+        Info: {
+          id: 21,
+          mainImage: "images/graptoveria_lovely_rose_1.jpg",
+          images: ["images/graptoveria_lovely_rose_1.jpg", "images/graptoveria_lovely_rose_2.jpg"],
+          title: "Graptoveria Lovely Rose (varias) + maceta + tierra",
+          description: "Graptoveria Lovely Rose (varias) + maceta + tierra",
+          price: "$2000"
+        }
+      },        
+      {
+        Info: {
+          id: 22,
+          mainImage: "images/suculenta_1.jpg",
+          images: ["images/suculenta_1.jpg", "images/suculenta_2.jpg"],
+          title: "Suculenta grande + maceta + tierra",
+          description: "Suculenta grande + maceta + tierra",
+          price: "$800"
+        }
+      }
+    ]
+  };
+
+
+function createProductCard(product) {
+    const articleContainer = document.createElement("article")
+    articleContainer.classList.add("style1")
+
+    const spanContainer = document.createElement("span")
+    spanContainer.classList.add("image")
+
+
+    const productImage = document.createElement("img");
+    productImage.src = product.Info.mainImage;
+    productImage.alt = product.Info.title;
+    spanContainer.appendChild(productImage);
+
+
+    const myH = document.createElement("h2");
+    myH.textContent = product.Info.title    
+
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("content")
+
+    const myDescription = document.createElement("p");
+    myDescription.textContent = product.Info.price;
+    myDiv.appendChild(myDescription);
+
+    const refContainer = document.createElement("a")
+    refContainer.href = "generic.html?productId="+product.Info.id
+    refContainer.appendChild(myH)
+    refContainer.appendChild(myDiv)
+
+    articleContainer.appendChild(spanContainer)
+    articleContainer.appendChild(refContainer)
+
+    return articleContainer;
+  }
+
+
+ function displayProducts(products) {
+    const productContainer = document.getElementById("product-container");
+    productContainer.innerHTML = ""; // Clear any existing content
+    const sectionContainer = document.createElement("section");
+    sectionContainer.classList.add("tiles")
+
+    products.forEach(product => {
+      const productCard = createProductCard(product);
+      sectionContainer.appendChild(productCard);
+    });
+    productContainer.appendChild(sectionContainer)
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    displayProducts(data_products.products);
+  });
+
+
+
+
+  function createDetailProduct(productId) {
+
+    data_products.products.forEach(product => {
+      if (product.id === productId) {
+        const productFound = product;        
+      }    
+    });
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("inner")
+
+    const articleContainer = document.createElement("h1")
+    articleContainer.textContent = productFound.Info.title
+
+
+    myDiv.appendChild(articleContainer);    
+
+    productFound.images.forEach(myImageUrl => {
+      const mySpan = document.createElement("span")
+      mySpan.classList.add("image main")
+      const productImage = document.createElement("img");
+      productImage.src = myImageUrl;  
+      productImage.style = "width: 80%";
+      mySpan.appendChild(productImage);     
+      myDiv.appendChild(mySpan);
+    });
+    
+    const myDescription = document.createElement("p");
+    myDescription.textContent = productFound.Info.description;
+    
+
+    myDiv.appendChild(myDescription);
+
+    return myDiv;
+  }
+
+   function displayDetailProduct(productId) {
+    const productContainer = document.getElementById("product-detail-container");
+    productContainer.innerHTML = "";
+   
+    const sectionContainer = createDetailProduct(productId);
+    
+    productContainer.appendChild(sectionContainer)
+  }
+
+
+  //document.addEventListener("DOMContentLoaded", function() {
+  //  displayDetailProduct(productId);
+  //});
